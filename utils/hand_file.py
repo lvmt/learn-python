e1c5d8451d40e1b30db07b611764f441accfec51
lvmt <13554221497@163.com>
2021-02-26 10:45:17 +0800

    类

--- utils/hand_file.py
+++ utils/hand_file.py
@@ -4,3 +4,23 @@
 # @Time   2021/2/26 10:28
 # @Email  13554221497@163.com
 # @File   hand_file.py
+
+
+"""
+如何流式读取大文件
+"""
+
+
+# 标准做法
+def count_nine(fname):
+    """
+    计算文件中含有多少个数字'9'
+    """
+    count = 0
+    with open(fname) as file:
+        for line in file:
+            count += line.count('9')
+    return count
+
+
+
